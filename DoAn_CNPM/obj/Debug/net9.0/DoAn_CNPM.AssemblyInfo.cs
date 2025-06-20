d73ecb6fcaf2aaca024e18c50a731ed317b2f613
Duong194 <aot2062004@gmail.com>
2025-06-20 14:03:47 +0700

    NguyenThaiDuong_2280600524_KhoiTao

--- DoAn_CNPM/obj/Debug/net9.0/DoAn_CNPM.AssemblyInfo.cs
+++ DoAn_CNPM/obj/Debug/net9.0/DoAn_CNPM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAn_CNPM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db1ca502a207bdbfbe828f3ddb57212c922c356")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAn_CNPM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAn_CNPM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
